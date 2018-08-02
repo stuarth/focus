0ab8400b826e340ad88d01a17bff21e182c80c03
Stuart Hinson <stuart.hinson@gmail.com>
2018-08-02 15:53:47 -0700

    apply rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod util {
     use std::fs::File;
-    use std::io::{Read};
+    use std::io::Read;
 
     pub fn slurp<P: AsRef<::std::path::Path>>(path: P) -> ::std::io::Result<String> {
         let mut s = String::new();
@@ -20,20 +20,31 @@ mod focus {
     const HEADER: &'static str = "FOCUS";
 
     pub fn enabled() -> bool {
-        util::slurp(PATH).expect("could not read /etc/hosts").contains(HEADER)
+        util::slurp(PATH)
+            .expect("could not read /etc/hosts")
+            .contains(HEADER)
     }
 
     pub fn enable() {
-        let sites_path = ::std::env::home_dir().map(|mut h| {h.push(".focus"); h}).expect("no home folder?");
+        let sites_path = ::std::env::home_dir()
+            .map(|mut h| {
+                h.push(".focus");
+                h
+            }).expect("no home folder?");
 
         let sites = util::slurp(&sites_path).expect("could not read ~/.focus");
 
-        let mut f = OpenOptions::new().append(true).open(PATH).expect("error opening");
+        let mut f = OpenOptions::new()
+            .append(true)
+            .open(PATH)
+            .expect("error opening");
         f.write("\n# FOCUS\n".as_bytes()).expect("error writing");
-        
+
         for domain in sites.lines() {
-            f.write(format!("127.0.0.1  {}\n", domain).as_bytes()).expect("error writing");
-            f.write(format!("127.0.0.1  www.{}\n", domain).as_bytes()).expect("error writing");
+            f.write(format!("127.0.0.1  {}\n", domain).as_bytes())
+                .expect("error writing");
+            f.write(format!("127.0.0.1  www.{}\n", domain).as_bytes())
+                .expect("error writing");
         }
         f.write("# END FOCUS\n".as_bytes()).expect("error writing");
     }
@@ -46,7 +57,11 @@ mod focus {
             .collect::<Vec<_>>()
             .join("\n");
 
-        let mut f = OpenOptions::new().write(true).truncate(true).open("/etc/hosts").expect("error opening -- did you run as sudo?");
+        let mut f = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .open("/etc/hosts")
+            .expect("error opening -- did you run as sudo?");
 
         f.write(content.as_bytes()).expect("error writing");
     }
@@ -60,15 +75,15 @@ fn main() {
         Some("enable") => {
             println!("MAXIMUM EFFORT!!");
             focus::enable();
-        },
+        }
         Some("disable") => {
             println!("SLACKER!");
             focus::disable();
-        },
+        }
         Some(unknown) => {
             println!("don't know '{}'", unknown);
-        },
-        None =>  {
+        }
+        None => {
             if focus::enabled() {
                 println!("SLACKER!");
                 focus::disable();
